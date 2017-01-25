e73cd7a6f53aa257f9f49dd62580b2d075e5522d
Taga-tumba <paul.amurao@gmail.com>
2017-01-25 12:23:53 -0500

    Create DCMC.cpp

---
+++ DCMC.cpp
@@ -0,0 +1,48 @@
+/* 
+   Digital Chaos Magic Channeler v1.0
+   by alcopaul
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string lib64 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnop-qrstuvwxyz+";
+	string msg;
+	string key;
+	int x = 0;
+	int y = 0;
+	string result = "";
+	cout << "Enter string : ";
+	cin >> msg;
+	key = msg;
+	for (x = 0; x < (int) key.length(); x++)
+	{
+		y += (int)key[x];
+	}
+	srand(y);
+	// anticollision
+    for (int j = 0; j < (int) key.length(); j++)
+	{
+		rand();
+	}
+	//
+	for (int e = 0; e < (int) msg.length(); e++)
+	{
+		int k = lib64.find(msg.substr(e, 1)) - rand() % 64;
+		if (k < 0)
+		{
+			k = k + 64;
+		}
+		result += lib64.substr(k, 1);
+	}
+	cout << result;
+	cout << endl;
+	cout << "Press any key and <enter> to exit\n";
+	string g;
+	cin >> g;
+	return 0;
+}
